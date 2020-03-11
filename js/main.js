b306226ace9f85303b920a4a0df465da90f907b0
zohairajweh <zohair.ajweh@hotmail.com>
2020-03-11 10:19:14 -0600

    findIndex

--- js/main.js
+++ js/main.js
@@ -2,11 +2,15 @@
 
 
 
-var myArray=['hello,word',2,3,true,5,null];
+var myArray=['Hello,Word',2,3,true,5,null,38];
 var myVlaue=myArray.find(function(element){return element>4;});
 console.log(myVlaue);
-
-var isMystringinside=myArray.find(function(element){return element==='hello,word';});
+var myVlaue=myArray.find(function(element){return element<4;});
+console.log(myVlaue);
+var isMystringinside=myArray.find(function(element){return element.toLowerCase() ==='hello,word';});
 console.log(isMystringinside);
 var isMystringinside=myArray.find(function(element){return element==='goodbye';});
 console.log(isMystringinside);
+
+var findAnIndex=myArray.findIndex(function(element){return element===2;});
+console.log(findAnIndex);
