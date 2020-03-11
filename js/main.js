a08de29c9e91b59e5228f3e431ece4599c64f273
zohairajweh <zohair.ajweh@hotmail.com>
2020-03-11 10:07:06 -0600

    method Find()

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-alert('sdsadsadsadsad');
+
+
+
+
+var myArray=['hello,word',2,3,true,5,null];
+var myVlaue=myArray.find(function(element){return element>4;});
+console.log(myVlaue);
+
+var isMystringinside=myArray.find(function(element){return element==='hello,word';});
+console.log(isMystringinside);
+var isMystringinside=myArray.find(function(element){return element==='goodbye';});
+console.log(isMystringinside);
