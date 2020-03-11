fbc46b3a6232a4c483fe4481f9a0c4dfc3a98c0d
zohairajweh <zohair.ajweh@hotmail.com>
2020-03-11 10:25:22 -0600

    entries

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
 
-
+// find()
 
 var myArray=['Hello,Word',2,3,true,5,null,38];
 var myVlaue=myArray.find(function(element){return element>4;});
@@ -11,6 +11,16 @@ var isMystringinside=myArray.find(function(element){return element.toLowerCase()
 console.log(isMystringinside);
 var isMystringinside=myArray.find(function(element){return element==='goodbye';});
 console.log(isMystringinside);
-
+// findAnIndex()
 var findAnIndex=myArray.findIndex(function(element){return element===2;});
 console.log(findAnIndex);
+
+
+
+// entries()
+var paring=myArray.entries();
+console.log(paring.next().value);
+console.log(paring.next().value);
+console.log(paring.next().value);
+console.log(paring.next().value);
+console.log(paring.next().value);
