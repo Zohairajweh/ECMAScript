62b5142436bbd51afa5530b5f4dea88b19d67633
zohairajweh <zohair.ajweh@hotmail.com>
2020-03-11 15:29:53 -0600

    get and set

--- js/main.js
+++ js/main.js
@@ -158,20 +158,35 @@ console.log(myBlocknestedVar);
  * Classes.
  * *** CHECK OUR IMPORT, THAT'S HOW WE'RE ACCESSING THIS!
  */
-
 class Movie {
-    constructor( name, genre, year ) {
+    constructor( name, genre, year ) { // We can use the constructor method to set up our properties. When we make a new Movie (object), we can pass in arguments this way!
         this.name = name;
         this.genre = genre;
         this.year = year;
+    }
+    get getName() { // How we retrieve the property. A GETTER.
+        return this.name;
+    }
+    set setName( name ) { // How we store a new property value. A SETTER.
+        if ( typeof name === 'string' ) {
+            this.name = name;
+        } else {
+            this.name = name.toString();
+        }
+    }
+    get getYear() {
+        return 'y'+this.year;
+    }
+    set setYear( year ) {
+        this.year = Number( year );
     }
     showPoster() { // This is a method! We can name them as we'd like.
         const info = `
             MOVIE INFO
             ==========
-            Name: ${this.name}
+            Name:  ${this.name}
             Genre: ${this.genre}
-            Year: ${this.year}
+            Year:  ${this.year}
         `;
         return info;
     }
@@ -184,4 +199,3 @@ const godfather = new Movie( 'Godfather II', 'Action/Thriller', 1974 );
 console.log( tron );
 console.log( dragonheart );
 console.log( godfather );
-    
